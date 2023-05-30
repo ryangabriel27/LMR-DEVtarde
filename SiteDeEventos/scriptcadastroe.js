fee8492fcc915bd68b8d87fecfa605626197dfd4
ryangabriel27 <123770312+ryangabriel27@users.noreply.github.com>
2023-05-30 23:09:27 +0000

    aaaa

--- SiteDeEventos/scriptcadastroe.js
+++ SiteDeEventos/scriptcadastroe.js
@@ -1,14 +1,29 @@
 function cadastroEvento() {
-    var nomeEvento, dataEvento, nParticipantes, nomePalestrante
+    var nomeEvento, dataEvento, descricaoEvento, localEvento;
 
     nomeEvento = document.getElementById("eNome").value;
     dataEvento = document.getElementById("eData").value;
-    nParticipantes = document.getElementById("eNumero").value;
-    nomePalestrante = document.getElementById("eNomeP").value;
+    descricaoEvento = document.getElementById("eDescricao").value;
+    localEvento = document.getElementById("eLocal").value;
 
-    if (nomeEvento != "" && nomePalestrante != "" && dataEvento !=  && nParticipantes > 0) {
 
-        alert(nomeEvento+" "+nomePalestrante+" "+dataEvento+" "+nParticipantes)
+
+    if (nomeEvento === "" || dataEvento === "" || descricaoEvento === "" || localEvento === "") {
+        alert("Para cadastrar o seu evento todos os campos devem ser preenchidos.");
+        return;
     }
+    var dataAtual = new Date().toISOString().split('T')[0]; // Obter a data atual
+    if (dataEvento < dataAtual) {
+        alert("A data do evento deve ser a partir do dia de hoje.");
+        return;
+    }
+    
+    alert(nomeEvento+" "+dataEvento+" "+descricaoEvento+" "+localEvento);
+    document.getElementById("eNome").value = "";
+    document.getElementById("eData").value = "";
+    document.getElementById("eDescricao").value = "";
+    document.getElementById("eLocal").value = "";
+
+    alert("Evento cadastrado com sucesso!");
 
 }
